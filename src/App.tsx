df981749bb40bbb93b41150f416dbbec3d0dd6d3
Maximus3333 <m.deisadze@hotmail.com>
2023-03-31 12:32:56 -0400

    removed draftjs code and cleaned up quill

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,8 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
-import {
-  EditorState,
-  ContentState,
-  convertFromHTML,
-  EditorBlock,
-  Modifier,
-  SelectionState,
-} from "draft-js";
-import Editor from "@draft-js-plugins/editor";
 import "draft-js/dist/Draft.css";
 import mammoth from "mammoth";
-import { NewAIConversation } from "./NewAIConversation";
-import createInlineToolbarPlugin from "@draft-js-plugins/inline-toolbar";
-import ReactQuill, { Quill } from "react-quill";
+import ReactQuill, { Quill, ReactQuillProps } from "react-quill";
 import "react-quill/dist/quill.snow.css";
-
-import {
-  ItalicButton,
-  BoldButton,
-  UnderlineButton,
-  CodeButton,
-  HeadlineOneButton,
-  HeadlineTwoButton,
-  HeadlineThreeButton,
-  UnorderedListButton,
-  OrderedListButton,
-  BlockquoteButton,
-  CodeBlockButton,
-} from "@draft-js-plugins/buttons";
 
 const resumeFileName = `http://localhost:3000/resume.docx`;
 
@@ -40,8 +15,6 @@ async function getHtml() {
     .convertToHtml({ arrayBuffer: arrBuffer })
     .then(function (result) {
       var html = result.value;
-      // var messages = result.messages;
-
       return html;
     })
     .catch((err) => console.log("error", err));
@@ -49,115 +22,39 @@ async function getHtml() {
   return html;
 }
 
-function EditorBlockWrapper(props: any) {
-  return (
-    <div>
-      <EditorBlock {...props} />
-    </div>
-  );
-}
-
-function blockRender(contentBlock: any) {
-  return {
-    component: EditorBlockWrapper,
-    editable: true,
-    props: {
-      foo: "bar",
-    },
-  };
-}
-
-const inlineToolbarPlugin = createInlineToolbarPlugin();
-
-const { InlineToolbar } = inlineToolbarPlugin;
-
 function MyEditor() {
-  const [editorState, setEditorState] = useState<EditorState>(() =>
-    EditorState.createEmpty()
-  );
-
-  const [selectedTextState, setSelectedTextState] = useState<string>("");
-  const [selectionStateGlobal, setSelectionState] = useState<SelectionState>();
-
-  // const [plugins, InlineToolbar] = useMemo(() => {
-  //   const inlineToolbarPlugin = createInlineToolbarPlugin();
-  //   return [[inlineToolbarPlugin], inlineToolbarPlugin.InlineToolbar];
-  // }, []);
-
   useEffect(() => {
     const html = getHtml().then((html) => {
-      // const blocksFromHTML = convertFromHTML(html || "");
-
-      // const state = ContentState.createFromBlockArray(
-      //   blocksFromHTML.contentBlocks,
-      //   blocksFromHTML.entityMap
-      // );
-
       handlePasteHTML(html);
-
-      // setEditorState(EditorState.createWithContent(state));
     });
   }, []);
-
-  const handleOnChange = (newState: EditorState) => {
-    
-  };
-
-  const handleUpdatePrompt = (editedText: string) => {
-    if (!selectionStateGlobal) {
-      return;
-    }
-
-    if (!editedText) {
-      return;
-    }
-
-    const contentState = editorState.getCurrentContent();
-
-    // Update the content state with the new text
-    const updatedContentState = Modifier.replaceText(
-      contentState,
-      selectionStateGlobal,
-      editedText
-    );
-
-    // Create a new editor state with the updated content state
-    const updatedEditorState = EditorState.push(
-      editorState,
-      updatedContentState,
-      "change-block-data"
-    );
-
-    setEditorState(updatedEditorState);
-  };
-
-  const component = () => {
-    return <button>Test</button>;
-  };
 
   const quillRef: any = React.useRef(null);
 
   const handlePasteHTML = (html: any) => {
+    console.log(quillRef.current);
+    console.log("yoo");
+    console.log("yoo");
+
     if (quillRef.current) {
       quillRef.current.getEditor().clipboard.dangerouslyPasteHTML(0, html);
+    }
+    console.log(quillRef.current.value);
+  };
+  const handleSelectionChange = (range: any) => {
+    console.log(range);
+
+    if (range) {
+      const text = quillRef.current
+        ?.getEditor()
+        .getText(range.index, range.length);
+      console.log("Selected text:", text);
     }
   };
 
   return (
     <>
       <h1> Chat GPT interaction</h1>
-
-      <p>Selected Text: {selectedTextState}</p>
-
-      <div style={{ position: "relative", height: "200px" }}>
-        {selectedTextState && (
-          <NewAIConversation
-            handleUpdatePrompt={handleUpdatePrompt}
-            selectedText={selectedTextState}
-          />
-        )}
-      </div>
-
       <h1> Editor</h1>
 
       <div
@@ -165,11 +62,7 @@ function MyEditor() {
           padding: "10%",
         }}
       >
-        {/* <Editor editorState={editorState} onChange={handleOnChange} blockRendererFn={blockRender} plugins={[inlineToolbarPlugin]} /> */}
-        {/* <InlineToolbar> */}
-        {/* {component} */}
-        {/* </InlineToolbar> */}
-        <ReactQuill ref={quillRef} />
+        <ReactQuill ref={quillRef} onChangeSelection={handleSelectionChange} />
       </div>
     </>
   );
