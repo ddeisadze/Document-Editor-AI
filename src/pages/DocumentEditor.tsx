e01e4685f073db79cc98dd3d2792953a0926ce7f
David Deisadze <tbilisidavid@hotmail.com>
2023-04-03 16:02:06 -0400

    Renames

--- src/pages/DocumentEditor.tsx
+++ src/pages/DocumentEditor.tsx
@@ -1,11 +1,7 @@
-import React, { FC, useCallback, useMemo, useRef } from "react";
-import mammoth from "mammoth";
-import { Quill, Range, ReactQuillProps } from "react-quill";
 import "react-quill/dist/quill.snow.css";
-import { EditorState, Modifier, SelectionState } from "draft-js";
 import "react-quill/dist/quill.snow.css";
-import "./App.css"
-import { ChakraProvider, VStack } from "@chakra-ui/react"
+import "./DocumentEditor.css"
+import { ChakraProvider } from "@chakra-ui/react"
 import { DocumentEditor } from "../components/documents/editor/DocumentEditor";
 
 function App() {
