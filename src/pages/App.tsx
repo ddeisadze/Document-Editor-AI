625533021fa7e93f5cbdb4f4207e0887d8c851a9
Maximus3333 <m.deisadze@hotmail.com>
2023-04-22 20:44:24 -0400

    pull

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,7 @@
 import { LinkedInCallback } from 'react-linkedin-login-oauth2';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DocumentEditorPage from './DocumentPage';
+import { DocumentManager } from '../components/documents/Gallery';
 
 export default function App() {
 
@@ -9,6 +10,8 @@ export default function App() {
             <Routes>
                 <Route path="/linkedin" element={<LinkedInCallback />} />
                 <Route path="/" Component={DocumentEditorPage} />
+                <Route path="/manager" Component={DocumentManager} />
+
             </Routes>
         </BrowserRouter>
     );
